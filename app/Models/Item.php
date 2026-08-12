cd09716046ab58990d2bee9da5355c4917731a87
LaurynasZuk <116870769+LaurynasZuk@users.noreply.github.com>
2026-08-12 16:11:48 +0300

    Use interval and combat bonuses on items

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -14,8 +14,10 @@ class Item extends Model
         'stack_limit',
         'equip_slot',
         'combat_style',
-        'attack_ticks',
-        'max_hit',
+        'attack_interval_ms',
+        'attack_bonus',
+        'strength_bonus',
+        'defence_bonus',
         'inventory_slots_bonus',
     ];
 
@@ -24,8 +26,10 @@ class Item extends Model
         return [
             'stackable' => 'boolean',
             'stack_limit' => 'integer',
-            'attack_ticks' => 'integer',
-            'max_hit' => 'integer',
+            'attack_interval_ms' => 'integer',
+            'attack_bonus' => 'integer',
+            'strength_bonus' => 'integer',
+            'defence_bonus' => 'integer',
             'inventory_slots_bonus' => 'integer',
         ];
     }
