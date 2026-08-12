7e9d0c41f69b608e234edccb0260805687da347d
LaurynasZuk <116870769+LaurynasZuk@users.noreply.github.com>
2026-08-12 12:10:55 +0300

    Remove starter sidebar from game page

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -14,6 +14,7 @@ createInertiaApp({
     layout: (name) => {
         switch (true) {
             case name === 'welcome':
+            case name === 'game':
                 return null;
             case name.startsWith('auth/'):
                 return AuthLayout;
