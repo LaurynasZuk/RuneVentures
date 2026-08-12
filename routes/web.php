f68266fe650182b23fbfff42f59498180186ad30
LaurynasZuk <116870769+LaurynasZuk@users.noreply.github.com>
2026-08-12 12:23:58 +0300

    Add location category pages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [GameController::class, 'show'])->name('dashboard');
+    Route::get('game/location/{location}/{category}', [GameController::class, 'locationCategory'])
+        ->name('game.location.category');
     Route::post('game/travel/{location}', [GameController::class, 'travel'])->name('game.travel');
     Route::post('game/actions/chop', [GameController::class, 'chop'])->name('game.chop');
     Route::post('game/actions/attack/{monster}', [GameController::class, 'attack'])->name('game.attack');
