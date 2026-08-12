bd8511f84907a50ce04aaa893a83ebe659cdb54f
LaurynasZuk <116870769+LaurynasZuk@users.noreply.github.com>
2026-08-12 15:25:54 +0300

    Add weapon combat timing fields

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -13,6 +13,9 @@ class Item extends Model
         'stackable',
         'stack_limit',
         'equip_slot',
+        'combat_style',
+        'attack_ticks',
+        'max_hit',
         'inventory_slots_bonus',
     ];
 
@@ -21,6 +24,8 @@ class Item extends Model
         return [
             'stackable' => 'boolean',
             'stack_limit' => 'integer',
+            'attack_ticks' => 'integer',
+            'max_hit' => 'integer',
             'inventory_slots_bonus' => 'integer',
         ];
     }
