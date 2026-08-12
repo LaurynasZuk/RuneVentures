97690b6d9084e3820737110c432d1711755b6b5b
LaurynasZuk <116870769+LaurynasZuk@users.noreply.github.com>
2026-08-12 15:27:02 +0300

    Route combat through timed combat controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CombatController;
 use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
         ->name('game.location.category');
     Route::post('game/travel/{location}', [GameController::class, 'travel'])->name('game.travel');
     Route::post('game/actions/chop', [GameController::class, 'chop'])->name('game.chop');
-    Route::post('game/actions/attack/{monster}', [GameController::class, 'attack'])->name('game.attack');
+
+    Route::post('game/actions/attack/{monster}', [CombatController::class, 'start'])->name('game.attack');
+    Route::get('game/combat', [CombatController::class, 'show'])->name('game.combat');
+    Route::get('game/combat/state', [CombatController::class, 'state'])->name('game.combat.state');
+    Route::post('game/combat/leave', [CombatController::class, 'leave'])->name('game.combat.leave');
 });
 
 require __DIR__.'/settings.php';
