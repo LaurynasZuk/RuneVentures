9a963f94dd13c08e9dfa433533ab91a0b43a7acd
LaurynasZuk <116870769+LaurynasZuk@users.noreply.github.com>
2026-08-12 17:28:16 +0300

    Rename dashboard route to main

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return auth()->check()
-        ? redirect()->route('dashboard')
+        ? redirect()->route('main')
         : redirect()->route('login');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', [GameController::class, 'show'])->name('dashboard');
+    Route::get('main', [GameController::class, 'show'])->name('main');
     Route::get('game/location/{location}/{category}', [GameController::class, 'locationCategory'])
         ->name('game.location.category');
     Route::post('game/travel/{location}', [GameController::class, 'travel'])->name('game.travel');
