dd12a66c67464fdc5c7ad9a245cb26759adb8ab8
LaurynasZuk <116870769+LaurynasZuk@users.noreply.github.com>
2026-08-12 09:27:49 +0300

    Redirect home to login or game dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/', function () {
+    return auth()->check()
+        ? redirect()->route('dashboard')
+        : redirect()->route('login');
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [GameController::class, 'show'])->name('dashboard');
